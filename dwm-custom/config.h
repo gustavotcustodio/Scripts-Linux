758d06d79d1fd4f6b1a6f1f234deca570562277a
Gustavo <gustavo.27c02@gmail.com>
2023-02-03 14:00:53 -0300

    dwm updated

--- dwm-custom/config.h
+++ dwm-custom/config.h
@@ -14,7 +14,7 @@ static unsigned int gappov    = 30;       /* vert outer gap between windows and 
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 0;        /* 0 means bottom bar */
+static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "monospace:size=11", "Ubuntu Mono Nerd Font:size=11", "FontAwesome:size=11" };
 static char normbgcolor[]           = "#000000";//"#222222";
 static char normbordercolor[]       = "#444444";
@@ -34,9 +34,9 @@ typedef struct {
 } Sp;
 
 /* tagging */
-static const char *tags[] = { " ", " ", " ", " ", " ", " ", " ", " ", " " };
+static const char *tags[] = { " ", " ", " ", " ", " ", "", " ", " ", " " };
 
-static const char *defaulttagapps[] = { "alacritty", "brave", "telegram-desktop", "music", "steam", "thunar", NULL, "news", "transmission-gtk" };
+static const char *defaulttagapps[] = { "alacritty", "brave", "telegram-desktop", "music", "steam", "discord", NULL, "news", "transmission-gtk" };
 
 //const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x30", NULL };
 // const char *spcmd2[] = {"st", "-n", "spcalc", "-f", "monospace:size=16", "-g", "50x20", "-e", "bc", "-lq", NULL };
